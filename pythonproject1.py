e232cbe284375c63d3a82c49bb5676bd6c5b64e7
johnkuriakose2312 <johnkuriakose4421@gmail.com>
2024-10-04 16:03:08 +0530

    my first python program

---
+++ pythonproject1.py
@@ -0,0 +1,36 @@
+#print ("hello\n                                         world")
+#if 5>2:
+    #print("five is greater than two")
+    #if 5>2:
+        #print("FIVE IS GREATER THAN TWO")
+          ##x="thomas"
+#print (x)
+'''x=float (4)
+print (x)
+x=int(4)
+print (x)
+x=str(5)
+print(x)
+x=float(8888)
+print(x)
+x="5"
+y="john"
+print(type(x))
+print(type(y))
+x=(888.888)
+print(type(x))
+x,y,z="orange","banana","apple"
+print(x,y,z)
+x=y=z="orage"
+print(x)
+print(y)
+print(z)
+print(x+y+z)
+username=input("enter username: ")
+print("username is :"+username)
+a="hello"
+b=" world"
+c= a + b
+print(c)'''
+a="Hello World"
+print(a[9])
