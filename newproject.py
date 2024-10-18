6c52e38065f7775aca4b703361c23d4e18461e34
johnkuriakose2312 <johnkuriakose4421@gmail.com>
2024-10-18 14:34:08 +0530

    Add files via upload

---
+++ newproject.py
@@ -0,0 +1,11 @@
+"""
+Author:John Kuriakose
+Date:October 17
+Description:A Python program that asks the user to input their name and age.
+Hello, John !,You are 18 years old
+
+"""
+
+name=input("Enter your name:")
+age=input("Enter Your Age:")
+print("Hello,",name+"!,You are",age,"years old")
