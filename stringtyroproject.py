12dfd3863c0a418091f2ce2f9dd3204951620561
johnkuriakose2312 <johnkuriakose4421@gmail.com>
2024-10-18 15:09:57 +0530

    Add files via upload
    string concatenation

---
+++ stringtyroproject.py
@@ -0,0 +1,12 @@
+"""
+Author:John Kuriakose
+Date:October 18
+Description: Python program that stores a string in a variable.
+Extract a specific part of the string (substring) and then concatenate it
+with another string. Finally, display the new string.
+
+"""
+str_one="Hello, World!"
+str_two=str_one[7:12]
+str_three=" Everyone!"
+print (str_two+str_three)
