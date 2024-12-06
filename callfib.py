f2260c59c600272d79daef8476ae31c0584ccbc3
johnkuriakose2312 <johnkuriakose4421@gmail.com>
2024-12-06 16:18:02 +0530

    Add files via upload
    Program to define a module to find Fibonacci Numbers and import the module to another program.

---
+++ callfib.py
@@ -0,0 +1,6 @@
+import fibonacci
+from fibonacci import fibonacci_num
+num=int(input("Enter no of values:"))
+num1=int(input("Enter a number:"))
+num2=int(input("Enter a number:"))
+print(fibonacci_num(num))
---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci_num(num):
+   list=[]
+   num1=0
+   num2=1
+   for i in range(num):
+      list.append(num1)
+      num1,num2=num2,num1+num2
+   return list
+
